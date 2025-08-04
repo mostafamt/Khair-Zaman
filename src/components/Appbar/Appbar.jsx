ad8f595552b8cdf6ec0266ca62af2dc3a3098230
mostafa <mostafa.kamel@almotahidaeducation.com>
2025-08-04 14:01:48 +0300

    init menu

--- src/components/Appbar/Appbar.jsx
+++ src/components/Appbar/Appbar.jsx
@@ -5,26 +5,29 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
+import Container from "@mui/material/Container";
 import MenuIcon from "@mui/icons-material/Menu";
 
 const Appbar = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            خير زمان
-          </Typography>
-        </Toolbar>
+        <Container maxWidth="sm">
+          <Toolbar sx={{ padding: "0 !important" }}>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Khair Zaman
+            </Typography>
+          </Toolbar>
+        </Container>
       </AppBar>
     </Box>
   );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import Container from "@mui/material/Container";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Container maxWidth="sm">
+      <div>
+        <h3>البيتزا الشرقي</h3>
+      </div>
+      <div>
+        <h3>البيتزا الايطالي</h3>
+      </div>
+      <div>
+        <h3>الفطير الحادق</h3>
+      </div>
+      <div>
+        <h3>حواوشي</h3>
+      </div>
+      <div>
+        <h3>الصاروخ</h3>
+      </div>
+      <div>
+        <h3>الفطائر</h3>
+      </div>
+      <div>
+        <h3>الفطير الحلو</h3>
+      </div>
+    </Container>
+  );
 };
 
 export default Home;
