d9178dff8ef1f7251a11f58cf2f2c0a3c145df5c
mostafa <mostafa.kamel@almotahidaeducation.com>
2025-08-04 16:23:08 +0300

    add checkboxes to every item in the menu

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import Container from "@mui/material/Container";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
 import { data } from "../../server/data";
+
+import styles from "./home.module.scss";
 
 const Home = () => {
   return (
     <Container maxWidth="sm">
       {data.map((item) => (
-        <div key={item.id}>
+        <div key={item.id} className={styles.group}>
           <h3>{item.label}</h3>
           {item?.children?.map((item) => (
-            <div key={item.key}>
-              <h6>{item.label}</h6>
-            </div>
+            <FormGroup key={item.id}>
+              <FormControlLabel control={<Checkbox />} label={item.label} />
+            </FormGroup>
           ))}
         </div>
       ))}
