76d57e6d421e6761d9facc8374232c8158ae5b71
mostafa <mostafa.kamel@almotahidaeducation.com>
2025-08-04 14:23:10 +0300

    fix Appbar casing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router";
-import Appbar from "./components/AppBar/AppBar";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import "./App.css";
 import Rtl from "./components/Rtl/Rtl";
+import Appbar from "./components/Appbar/Appbar";
 
 function App() {
   return (
