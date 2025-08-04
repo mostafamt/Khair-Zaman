b6f58156ad42e744357e92b448d48f6290f37800
mostafa <mostafa.kamel@almotahidaeducation.com>
2025-08-04 12:22:24 +0300

    add react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1>Khair Zaman</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
